63c883b8dfe111641ed82b560fa5af7e8b797b1b
seekplum <1131909224@qq.com>
2019-04-03 22:49:34 +0800

    tasks中增加clean需要删除的文件

--- tasks.py
+++ tasks.py
@@ -10,10 +10,17 @@ name = 'plum_tools'
 
 @task
 def clean(ctx):
+    """清除代码中无效文件
+    """
     ctx.run('rm -rf build dist', echo=True)
     ctx.run("find . -name '*.pyc' -exec rm -f {} +", echo=True)
     ctx.run("find . -name '*.pyo' -exec rm -f {} +", echo=True)
     ctx.run("find . -name '__pycache__' -exec rm -rf {} +", echo=True)
+    ctx.run("find . -name 'htmlcov' -exec rm -rf {} +", echo=True)
+    ctx.run("find . -name '.coverage*' -exec rm -rf {} +", echo=True)
+    ctx.run("find . -name '.pytest_cache' -exec rm -rf {} +", echo=True)
+    ctx.run("find . -name '.benchmarks' -exec rm -rf {} +", echo=True)
+    ctx.run("find . -name '*.egg-info' -exec rm -rf {} +", echo=True)
 
 
 @task(clean)
@@ -21,21 +28,34 @@ def sdist(ctx):
     ctx.run('python setup.py sdist', echo=True)
 
 
-@task(clean)
+@task(sdist)
 def upload(ctx, r="private"):
+    """上传包到指定pip源
+    """
     ctx.run('python setup.py sdist upload -r %s' % r, echo=True)
 
 
-@task(clean)
+@task(sdist)
 def register(ctx, n, r="private"):
     ctx.run('twine register %s -r %s' % (n, r), echo=True, warn=True)
 
 
-@task(clean)
+@task(register)
 def tupload(ctx, n, r="private"):
+    """上传包到指定pip源
+    """
     ctx.run('twine upload %s -r %s' % (n, r), echo=True)
 
 
 @task(clean)
 def check(ctx, j=4):
+    """检查代码规范
+    """
     ctx.run("pylint -j %s --output-format colorized   --disable=all --enable=E,F plum_tools" % j)
+
+
+@task(clean)
+def unittest(ctx):
+    """运行单元测试和计算测试覆盖率
+    """
+    ctx.run("export PYTHONPATH=`pwd` && pytest --cov=plum_tools tests", echo=True)
