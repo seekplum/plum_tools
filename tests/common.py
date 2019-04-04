8e407a1e8f35d24183f0e5c11ebd7ff7b27193a7
seekplum <1131909224@qq.com>
2019-04-04 13:38:41 +0800

    完成git模块的单元测试

--- tests/common.py
+++ tests/common.py
@@ -2,6 +2,38 @@
 # -*- coding: utf-8 -*-
 
 import os
+import tempfile
+import shutil
+
+from contextlib import contextmanager
 
 curr_path = os.path.dirname(os.path.abspath(__file__))
 fixtures_path = os.path.join(curr_path, "fixtures")
+
+
+@contextmanager
+def make_temp_dir(prefix="plum_tools_", clean=True):
+    """
+    创建临时文件夹
+    clean: True 在with语句之后删除文件夹
+    """
+    temp_dir = tempfile.mkdtemp(prefix=prefix)
+    try:
+        yield temp_dir
+    finally:
+        if clean:
+            shutil.rmtree(temp_dir)
+
+
+@contextmanager
+def make_temp_file(suffix="", prefix="plum_tools_", clean=True):
+    """
+    创建临时文件
+    clean: True 在with语句之后删除文件夹
+    """
+    temp_file = tempfile.mktemp(suffix=suffix, prefix=prefix)
+    try:
+        yield temp_file
+    finally:
+        if clean and os.path.isfile(temp_file):
+            os.remove(temp_file)
--- tests/test_main.py
+++ tests/test_main.py
@@ -13,7 +13,7 @@ from tests.common import curr_path
 def main():
     print("测试目录: {}".format(curr_path))
 
-    file_path = os.path.join(curr_path, "", "test_conf.py")
+    file_path = os.path.join(curr_path, "utils", "test_git.py::test_check_repository_stash")
     if len(sys.argv) > 1:
         args = [sys.argv[1:]]
     elif (file_path.endswith(".py") and os.path.exists(file_path)) or (not file_path.endswith(".py")):
