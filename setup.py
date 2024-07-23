82a16950f36f222841cb65500fe7a46c126a2d07
seekplum <1131909224@qq.com>
2024-07-23 23:38:52 +0800

    MOD: setup配置移到pyproject中

--- setup.py
+++ setup.py
@@ -6,44 +6,12 @@ import shutil
 from plum_tools.conf import PathConfig
 from setuptools import find_packages, setup
 
-version = "0.3.0"
-
 
 def install():
     setup(
-        author="seekplum",
-        author_email="1131909224m@sina.cn",
-        classifiers=[
-            "Programming Language :: Python",
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.8",
-            "Programming Language :: Python :: 3.9",
-            "Programming Language :: Python :: 3.10",
-            "Programming Language :: Python :: 3.11",
-            "Programming Language :: Python :: 3.12",
-        ],
-        description="linux下常用的工具包",
-        install_requires=[
-            "setuptools",
-            "pyyaml",
-            "paramiko",
-        ],
         setup_requires=[],
         include_package_data=True,
-        name="plum_tools",
-        namespace_packages=["plum_tools"],
         packages=find_packages(exclude=["tests", "docs"]),
-        version=version,
-        entry_points={
-            "console_scripts": [
-                "gitrepo = plum_tools.gitrepo:main",
-                "gitstash = plum_tools.gitstash:main",
-                "pssh = plum_tools.pssh:main",
-                "pping = plum_tools.pping:main",
-                "pipmi = plum_tools.pipmi:main",
-                "prn = plum_tools.prn:main",
-            ],
-        },
     )
 
 
